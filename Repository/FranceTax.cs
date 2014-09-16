69a2b5ae8a9ce1f7da8fb3a832dd7987b17b16b0
Akilgour <kilgourkilgour@hotmail.com>
2014-09-16 22:40:28 +0100

    Added france tax and a test

---
+++ Repository/FranceTax.cs
@@ -0,0 +1,29 @@
+﻿using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+   public class FranceTax : ICalculateTax
+    {
+
+       public FranceTax(int Pay)
+       {
+           this.Pay = Pay;
+           TaxRate = 20;
+       }
+       public int TaxRate
+       { get; private set; }
+
+       public int Pay
+       { get; private set; }
+
+       public int CalculateTax()
+       {
+           return Pay - TaxRate;
+       }
+    }
+}
---
+++ UnitTestProject1/FranceTaxTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class FranceTaxTest
+    {
+        [TestMethod]
+        public void CalculateTax_AreEqual()
+        {
+            //Arange
+            var company = new FranceTax(100);
+
+            //Act
+            var value = company.CalculateTax();
+
+            //Assert
+            Assert.AreEqual(value, 80);
+
+        }
+    }
+}
