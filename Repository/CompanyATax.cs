26949ddde4116c9bd4a7e2a3d6343512ace63688
Akilgour <kilgourkilgour@hotmail.com>
2014-09-16 21:47:27 +0100

    Added Comapany A and a unit test for it

---
+++ Repository/CompanyATax.cs
@@ -0,0 +1,29 @@
+﻿using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CompanyATax : ICalculateTax
+    {
+        public CompanyATax(int Pay)
+        {
+            this.Pay = Pay;
+            TaxRate = 0;
+        }
+
+        public int TaxRate
+        { get; private set; }
+
+        public int Pay
+        { get; private set; }
+
+        public int CalculateTax()
+        {
+            return Pay - TaxRate;
+        }
+    }
+}
---
+++ UnitTestProject1/CompanyATaxTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+ 
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CompanyATaxTest
+    {
+        [TestMethod]
+        public void CalculateTax_AreEqual()
+        {
+            //Arange
+            var company = new CompanyATax(100); 
+
+            //Act
+            var value = company.CalculateTax();
+
+            //Assert
+            Assert.AreEqual(value, 100);
+
+        }
+    }
+}
