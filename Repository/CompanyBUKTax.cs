eb398b5697d5aa37c2d6082171691c8a198e9f50
Akilgour <kilgourkilgour@hotmail.com>
2014-09-16 22:05:44 +0100

    Added company B UK tax and a test for it

---
+++ Repository/CompanyBUKTax.cs
@@ -0,0 +1,30 @@
+﻿using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+   public class CompanyBUKTax : ICalculateTax
+    {
+       private int p;
+
+       public CompanyBUKTax(int Pay)
+       {
+           this.Pay = Pay;
+           TaxRate = 10;
+       }
+       public int TaxRate
+       { get; private set; }
+
+       public int Pay
+       { get; private set; }
+
+       public int CalculateTax()
+       {
+           return Pay - TaxRate;
+       }
+    }
+}
---
+++ UnitTestProject1/CompanyBUKTaxTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CompanyBUKTaxTest
+    {
+        [TestMethod]
+        public void CalculateTax_AreEqual()
+        {
+            //Arange
+            var company = new CompanyBUKTax(100); 
+
+            //Act
+            var value = company.CalculateTax();
+
+            //Assert
+            Assert.AreEqual(value, 90);
+
+        }
+    }
+}
