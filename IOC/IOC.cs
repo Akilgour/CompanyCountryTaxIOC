6c327da7e7769db14142a6e861d1700a69e82a6c
Akilgour <kilgourkilgour@hotmail.com>
2014-09-17 20:16:13 +0100

    Added IOC, just created and stubbed it out, just first pass

---
+++ IOC/IOC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC
+{
+    public class IOC
+    {
+       
+        public IOC()
+        {
+            Items = new List<IOCItemcs>();
+        }
+
+        public List<IOCItemcs> Items { get; set; }
+
+    }
+}
